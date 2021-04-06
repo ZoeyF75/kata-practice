2532046863b60f034bce68104b0e1276d007b23b
Zoey Farago <zoeyfarago@Zoeys-MacBook-Pro.local>
2021-04-05 20:07:26 -0700

    comments

--- fibbonaci.js
+++ fibbonaci.js
@@ -8,9 +8,9 @@ let nthTerm;
 const fibbonaci = (n) => {
   lastTerm = sequence[sequence.length - 1];
   sequence[sequence.length - 2] === undefined ? secondLast = 0 : secondLast = sequence[sequence.length - 2];
-  if (nth === n) {
+  if (nth === n) { //if the nth count is equal to n then return last value in sequence
     nthTerm = sequence[nth - 1];
-  } else {
+  } else { //add the last two terms to the seqence continuously using recursion
     sequence.push(lastTerm + secondLast);
     nth++;
     fibbonaci(n);
